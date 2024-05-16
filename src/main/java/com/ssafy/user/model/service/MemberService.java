2c28c3bf13151c44c473303b0614aafac6546545
최태민 <wxogud3377@naver.com>
2024-05-16 13:45:04 +0900

    [ADD] : Login Function

--- src/main/java/com/ssafy/user/model/service/MemberService.java
+++ src/main/java/com/ssafy/user/model/service/MemberService.java
@@ -1,5 +1,8 @@
 package com.ssafy.user.model.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,28 @@ public class MemberService {
 	
 	public MemberDTO login(MemberDTO member) {
 		return mdao.selectOne(member);
+	}
+	
+	public void saveRefreshToken(String userId, String refreshToken) throws Exception {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("userId", userId);
+		map.put("token", refreshToken);
+		mdao.saveRefreshToken(map);
+	}
+	
+	public MemberDTO userInfo(String userId) throws Exception {
+		return mdao.userInfo(userId);
+	}
+	
+	public Object getRefreshToken(String userId) throws Exception {
+		return mdao.getRefreshToken(userId);
+	}
+	
+	public void deleRefreshToken(String userId) throws Exception {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("userId", userId);
+		map.put("token", null);
+		mdao.deleteRefreshToken(map);
 	}
 	
 	public boolean register(MemberDTO member) {
