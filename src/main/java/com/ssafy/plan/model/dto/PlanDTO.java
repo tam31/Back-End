e05ea3ad766070f24bc7331a07302f2e9b0340a7
조영서 <anijam12@naver.com>
2024-05-16 17:48:12 +0900

    [ADD] : Schedule API

--- src/main/java/com/ssafy/plan/model/dto/PlanDTO.java
+++ src/main/java/com/ssafy/plan/model/dto/PlanDTO.java
@@ -1,17 +1,21 @@
 package com.ssafy.plan.model.dto;
 
+import java.util.List;
+
 public class PlanDTO {
 	private int planIdx;
 	private String planTitle;
 	private String planDate;
+	private List<ScheduleDTO> schedules;
 	
 	public PlanDTO() {}
 
-	public PlanDTO(int planIdx, String planTitle, String planDate) {
+	public PlanDTO(int planIdx, String planTitle, String planDate, List<ScheduleDTO> schedules) {
 		super();
 		this.planIdx = planIdx;
 		this.planTitle = planTitle;
 		this.planDate = planDate;
+		this.schedules = schedules;
 	}
 
 	public int getPlanIdx() {
@@ -38,8 +42,17 @@ public class PlanDTO {
 		this.planDate = planDate;
 	}
 
+	public List<ScheduleDTO> getSchedules() {
+		return schedules;
+	}
+
+	public void setSchedules(List<ScheduleDTO> schedules) {
+		this.schedules = schedules;
+	}
+
 	@Override
 	public String toString() {
-		return "PlanDTO [planIdx=" + planIdx + ", planTitle=" + planTitle + ", planDate=" + planDate + "]";
+		return "PlanDTO [planIdx=" + planIdx + ", planTitle=" + planTitle + ", planDate=" + planDate + ", schedules="
+				+ schedules + "]";
 	}
 }
